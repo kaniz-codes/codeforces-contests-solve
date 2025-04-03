a3f4b3e37825f0200ddabb76f75ad577b31a78af
Kaniz Fatema <social.kaniz@gmail.com>
2025-04-03 17:12:56 +0600

    Updated

---
+++ 03_kotlin-heroes-practice-12/a_easy-problem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        int result = (n - 1);
+        cout << result << endl;
+    }
+
+    return 0;
+}
---
+++ 03_kotlin-heroes-practice-12/a_easy-problem.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val t = readLine()!!.toInt()
+
+    repeat(t) 
+    {
+        val n = readLine()!!.toInt()
+        val result = n - 1
+        println(result)
+    }
+}
